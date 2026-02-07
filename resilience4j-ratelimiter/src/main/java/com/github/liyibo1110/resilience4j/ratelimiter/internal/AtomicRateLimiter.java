54f18168c26d171ddacc794177390b2c717ce312
liyibo1110 <liyibo1110@sina.com>
2026-02-08 00:42:17 +0800

    更新rateLimiter模块内容

--- resilience4j-ratelimiter/src/main/java/com/github/liyibo1110/resilience4j/ratelimiter/internal/AtomicRateLimiter.java
+++ resilience4j-ratelimiter/src/main/java/com/github/liyibo1110/resilience4j/ratelimiter/internal/AtomicRateLimiter.java
@@ -2,9 +2,12 @@ package com.github.liyibo1110.resilience4j.ratelimiter.internal;
 
 import com.github.liyibo1110.resilience4j.ratelimiter.RateLimiter;
 import com.github.liyibo1110.resilience4j.ratelimiter.RateLimiterConfig;
+import io.vavr.collection.HashMap;
 import io.vavr.collection.Map;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 将从纪元开始以来的所有纳秒数拆分为周期，每个周期持续时间为limitRefreshPeriod纳秒。
@@ -14,6 +17,31 @@ import java.time.Duration;
  * @date 2026-02-06 23:33
  */
 public class AtomicRateLimiter implements RateLimiter {
+    /** AtomicRateLimiter实例的生成时间点 */
+    private final long nanoTimeStart;
+    private final String name;
+
+    /** 等候许可的线程总数 */
+    private final AtomicInteger waitingThreads;
+
+    /** 内置了状态对象（3个字段） */
+    private final AtomicReference<State> state;
+    private final Map<String, String> tags;
+    private final RateLimiterEventProcessor eventProcessor;
+
+    public AtomicRateLimiter(String name, RateLimiterConfig config) {
+        this(name, config, HashMap.empty());
+    }
+
+    public AtomicRateLimiter(String name, RateLimiterConfig config, Map<String, String> tags) {
+        this.name = name;
+        this.tags = tags;
+        this.nanoTimeStart = System.nanoTime();
+        this.waitingThreads = new AtomicInteger(0);
+        this.state = new AtomicReference<>(new State(config, 0, config.getLimitForPeriod(), 0));
+        this.eventProcessor = new RateLimiterEventProcessor();
+    }
+
     @Override
     public void changeTimeoutDuration(Duration timeoutDuration) {
 
@@ -62,5 +90,44 @@ public class AtomicRateLimiter implements RateLimiter {
     @Override
     public EventPublisher getEventPublisher() {
         return null;
+    }
+
+    /**
+     * 表示这个RateLimiter实现类的状态
+     */
+    private static class State {
+        private final RateLimiterConfig config;
+
+        /** 上一次调用acquirePermission()时，使用的周期数，这个值如果变了，则许可将要恢复重置 */
+        private final long activeCycle;
+
+        /** 上一次调用acquirePermission()后，可用的许可数量，如果某些许可被预支，该值可能为负数 */
+        private final int activePermissions;
+
+        /** 上一次调用acquirePermission()时，需要等待获取许可的纳秒数 */
+        private final long nanosToWait;
+
+        private State(RateLimiterConfig config, final long activeCycle, final int activePermissions, final long nanosToWait) {
+            this.config = config;
+            this.activeCycle = activeCycle;
+            this.activePermissions = activePermissions;
+            this.nanosToWait = nanosToWait;
+        }
+    }
+
+    public class AtomicRateLimiterMetrics implements Metrics {
+
+        private AtomicRateLimiterMetrics() {}
+
+        @Override
+        public int getNumberOfWaitingThreads() {
+            return waitingThreads.get();
+        }
+
+        @Override
+        public int getAvailablePermissions() {
+            State currentState = state.get();
+            return 0;
+        }
     }
 }
--- resilience4j-ratelimiter/src/main/java/com/github/liyibo1110/resilience4j/ratelimiter/internal/SemaphoreBasedRateLimiter.java
+++ resilience4j-ratelimiter/src/main/java/com/github/liyibo1110/resilience4j/ratelimiter/internal/SemaphoreBasedRateLimiter.java
@@ -1,12 +1,23 @@
 package com.github.liyibo1110.resilience4j.ratelimiter.internal;
 
+import com.github.liyibo1110.resilience4j.core.lang.Nullable;
 import com.github.liyibo1110.resilience4j.ratelimiter.RateLimiter;
 import com.github.liyibo1110.resilience4j.ratelimiter.RateLimiterConfig;
+import com.github.liyibo1110.resilience4j.ratelimiter.event.RateLimiterOnDrainedEvent;
+import com.github.liyibo1110.resilience4j.ratelimiter.event.RateLimiterOnFailureEvent;
+import com.github.liyibo1110.resilience4j.ratelimiter.event.RateLimiterOnSuccessEvent;
+import io.vavr.collection.HashMap;
 import io.vavr.collection.Map;
+import io.vavr.control.Option;
 
+import java.nio.file.Path;
 import java.time.Duration;
+import java.util.Objects;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -26,54 +37,154 @@ public class SemaphoreBasedRateLimiter implements RateLimiter {
     private final Map<String, String> tags;
     private final RateLimiterEventProcessor eventProcessor;
 
+    public SemaphoreBasedRateLimiter(final String name, final RateLimiterConfig rateLimiterConfig) {
+        this(name, rateLimiterConfig, HashMap.empty());
+    }
+
+    public SemaphoreBasedRateLimiter(final String name, final RateLimiterConfig rateLimiterConfig,
+                                     Map<String, String> tags) {
+        this(name, rateLimiterConfig, null, tags);
+    }
+
+    public SemaphoreBasedRateLimiter(String name, RateLimiterConfig rateLimiterConfig,
+                                     @Nullable ScheduledExecutorService scheduler) {
+        this(name, rateLimiterConfig, scheduler, HashMap.empty());
+    }
+
+    public SemaphoreBasedRateLimiter(String name, RateLimiterConfig rateLimiterConfig,
+                                     @Nullable ScheduledExecutorService scheduler, Map<String, String> tags) {
+        this.name = Objects.requireNonNull(name, NAME_MUST_NOT_BE_NULL);
+        this.rateLimiterConfig = new AtomicReference<>(Objects.requireNonNull(rateLimiterConfig, CONFIG_MUST_NOT_BE_NULL));
+        this.scheduler = Option.of(scheduler).getOrElse(this::configureScheduler);
+        this.tags = tags;
+        this.semaphore = new Semaphore(this.rateLimiterConfig.get().getLimitForPeriod(), true);
+        this.metrics = new SemaphoreBasedRateLimiterMetrics();
+        this.eventProcessor = new RateLimiterEventProcessor();
+        this.scheduleLimitRefresh();
+    }
+
+    /**
+     * 生成默认的延迟线程连接池
+     */
+    private ScheduledExecutorService configureScheduler() {
+        ThreadFactory factory = r -> {
+            Thread thread = new Thread(r, "SchedulerForSemaphoreBasedRateLimiterImpl-" + name);
+            thread.setDaemon(true);
+            return thread;
+        };
+        return Executors.newSingleThreadScheduledExecutor(factory);
+    }
+
+    /**
+     * 定期运行refreshLimit方法
+     */
+    private void scheduleLimitRefresh() {
+        this.scheduler.scheduleAtFixedRate(
+            this::refreshLimit,
+            this.rateLimiterConfig.get().getLimitRefreshPeriod().toNanos(),
+            this.rateLimiterConfig.get().getLimitRefreshPeriod().toNanos(),
+            TimeUnit.NANOSECONDS);
+    }
+
+    /**
+     * 重置许可总数（在scheduler里定期运行）
+     */
+    void refreshLimit() {
+        int permissionsToRelease = this.rateLimiterConfig.get().getLimitForPeriod() - this.semaphore.availablePermits();
+        this.semaphore.release(permissionsToRelease);
+    }
+
     @Override
     public void changeTimeoutDuration(Duration timeoutDuration) {
-
+        RateLimiterConfig newConfig = RateLimiterConfig.from(this.rateLimiterConfig.get())
+                .timeoutDuration(timeoutDuration)
+                .build();
+        this.rateLimiterConfig.set(newConfig);
     }
 
     @Override
     public void changeLimitForPeriod(int limitForPeriod) {
+        RateLimiterConfig newConfig = RateLimiterConfig.from(this.rateLimiterConfig.get())
+                .limitForPeriod(limitForPeriod)
+                .build();
+        this.rateLimiterConfig.set(newConfig);
+    }
 
+    /**
+     * 核心方法：尝试获取n个许可，注意方法只会返回bool来表示获取是否成功
+     */
+    @Override
+    public boolean acquirePermission(int permits) {
+        try {
+            boolean success = this.semaphore.tryAcquire(permits, rateLimiterConfig.get().getTimeoutDuration().toNanos(), TimeUnit.NANOSECONDS);
+            this.publishRateLimiterAcquisitionEvent(success, permits);
+            return success;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            this.publishRateLimiterAcquisitionEvent(false, permits);
+            return false;
+        }
     }
 
     @Override
-    public boolean acquirePermission(int permits) {
-        return false;
+    public long reservePermission() {
+        throw new UnsupportedOperationException("Reserving permissions is not supported in the semaphore based implementation");
     }
 
     @Override
     public long reservePermission(int permits) {
-        return 0;
+        throw new UnsupportedOperationException("Reserving permissions is not supported in the semaphore based implementation");
     }
 
     @Override
     public void drainPermissions() {
-
+        // 还是利用了semaphore自身的drainAPI
+        int permits = this.semaphore.drainPermits();
+        if(this.eventProcessor.hasConsumers())
+            this.eventProcessor.consumeEvent(new RateLimiterOnDrainedEvent(this.name, permits));
     }
 
     @Override
     public String getName() {
-        return null;
-    }
-
-    @Override
-    public RateLimiterConfig getRateLimiterConfig() {
-        return null;
-    }
-
-    @Override
-    public Map<String, String> getTags() {
-        return null;
+        return this.name;
     }
 
     @Override
     public Metrics getMetrics() {
-        return null;
+        return this.metrics;
     }
 
     @Override
     public EventPublisher getEventPublisher() {
-        return null;
+        return this.eventProcessor;
+    }
+
+    @Override
+    public RateLimiterConfig getRateLimiterConfig() {
+        return this.rateLimiterConfig.get();
+    }
+
+    @Override
+    public String toString() {
+        return "SemaphoreBasedRateLimiter{"
+                + "name='" + name + '\''
+                + ", rateLimiterConfig=" + rateLimiterConfig
+                + '}';
+    }
+
+    @Override
+    public Map<String, String> getTags() {
+        return tags;
+    }
+
+    private void publishRateLimiterAcquisitionEvent(boolean permissionAcquired, int permits) {
+        if(!this.eventProcessor.hasConsumers())
+            return;
+        if(permissionAcquired) {
+            this.eventProcessor.consumeEvent(new RateLimiterOnSuccessEvent(this.name, permits));
+            return;
+        }
+        this.eventProcessor.consumeEvent(new RateLimiterOnFailureEvent(this.name, permits));
     }
 
     private final class SemaphoreBasedRateLimiterMetrics implements Metrics {
